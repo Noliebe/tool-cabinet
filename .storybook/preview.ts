1f3a85eac2dd77f1059a0825c5109c03b6d8332b
noliebe <noliebe@qq.com>
2025-04-25 15:40:12 +0800

    fix: 修复

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,7 @@
 import type { Preview } from '@storybook/vue3'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/el-select.css'
-import './tailwind.css'
+// import './tailwind.css'
 // import "tailwindcss/index.css";
 // @tailwind base;
 // @tailwind components;
